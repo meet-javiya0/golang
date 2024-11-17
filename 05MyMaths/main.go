ded883d56f96601a1a6cea6f55931d2e9db0ac0c
Meet Javiya <107299231+meet-javiya0@users.noreply.github.com>
2024-11-17 18:07:51 +0530

    updated file

--- 05MyMaths/main.go
+++ 05MyMaths/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+	// "math/rand"
+	"crypto/rand"
+)
 
 func main() {
 	fmt.Println("Welcome to maths i golang")
@@ -8,6 +13,16 @@ func main() {
 	var mynumberOne int = 2
 	var mynumberTwo float64 = 4
 
-	// fmt.Println("The sum of these two number is: ", mynumberOne+int(mynumberTwo))
-	
+	fmt.Println("The sum of these two number is: ", mynumberOne+int(mynumberTwo))
+
+	// random number
+
+	// using math
+	// rand.Seed(time.Now().UnixNano())	// this is deprecated
+	// rand.New(rand.NewSource(5))
+	// fmt.Println(rand.Intn(5) + 1)
+
+	// using crypto
+	myRandomNumber, _ := rand.Int(rand.Reader, big.NewInt(5))
+	fmt.Println(myRandomNumber)
 }
